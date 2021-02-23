d4dd72f1e7b188ea6e8fb100dfea2232b4760c97
Jonas <j.g.f.pettersson@gmail.com>
2021-02-23 18:07:51 +0100

    follow

--- apps/oinkerprofile/views.py
+++ apps/oinkerprofile/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
 def oinkerprofile(request, username):
@@ -10,3 +11,21 @@ def oinkerprofile(request, username):
     }
 
     return render(request, 'oinkerprofile/oinkerprofile.html', context)
+
+
+@login_required
+def follow_oinker(request, username):
+    user = get_object_or_404(User, username=username)
+
+    request.user.oinkerprofile.follows.add(user.oinkerprofile)
+
+    return redirect('oinkerprofile', username=username)
+
+
+@login_required
+def unfollow_oinker(request, username):
+    user = get_object_or_404(User, username=username)
+
+    request.user.oinkerprofile.follows.remove(user.oinkerprofile)
+
+    return redirect('oinkerprofile', username=username)
